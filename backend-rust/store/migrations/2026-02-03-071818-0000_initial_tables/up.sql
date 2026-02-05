bb2b6928604c9fce8e5c493aeb7bb04ee7aecfe0
rohitJung <rokshh28@gmail.com>
2026-02-05 19:47:05 +0545

    feat: initial tables migrations

--- backend-rust/store/migrations/2026-02-03-071818-0000_initial_tables/up.sql
+++ backend-rust/store/migrations/2026-02-03-071818-0000_initial_tables/up.sql
@@ -1,1 +1,44 @@
 -- Your SQL goes here
+-- CreateEnum
+CREATE TYPE "website_status" AS ENUM (
+    'Up',
+    'Down',
+    'Unknown'
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" text NOT NULL,
+    "username" text NOT NULL,
+    "password" text NOT NULL,
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "website" (
+    "id" text NOT NULL,
+    "url" text NOT NULL,
+    "time_added" timestamp(3) NOT NULL,
+    "user_id" text NOT NULL,
+    CONSTRAINT "Website_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "website_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "region" (
+    "id" text NOT NULL,
+    "name" text NOT NULL,
+    CONSTRAINT "Region_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "website_tick" (
+    "id" text NOT NULL,
+    "response_time_ms" integer NOT NULL,
+    "status" "website_status" NOT NULL,
+    "region_id" text NOT NULL,
+    "website_id" text NOT NULL,
+    "created_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "website_tick_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "website_tick_website_id_fkey" FOREIGN KEY ("website_id") REFERENCES "website" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
