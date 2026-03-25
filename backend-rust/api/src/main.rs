8064fd9f3ffd23d9970e75251a4978da33617ffc
rohitJung <rokshh28@gmail.com>
2026-03-25 17:50:41 +0545

    refactor: moved code

--- backend-rust/api/src/main.rs
+++ backend-rust/api/src/main.rs
@@ -1,38 +1,25 @@
-use poem::{
-    get, handler,
-    listener::TcpListener,
-    web::{Json, Path},
-    Route, Server,
-};
-use serde::{Deserialize, Serialize};
+use poem::{get, handler, listener::TcpListener, post, web::Path, Route, Server};
+
+pub mod routes;
+pub mod types;
+
+use routes::user;
+
+use crate::routes::user::{signin_user, signup_user};
 
 #[handler]
 fn hello(Path(name): Path<String>) -> String {
     format!("hello: {}", name)
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct UserSignInResponse {
-    id: String,
-    token: String,
-}
-
-#[handler]
-fn signin_user() -> Json<UserSignInResponse> {
-    let response = UserSignInResponse {
-        id: String::from("id"),
-        token: String::from("token"),
-    };
-
-    Json(response)
-}
-
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
-        .at("/hello/:name", get(hello))
-        .at("/user/signin", get(signin_user));
+        .at("/user/signin", get(signin_user))
+        .at("/user/signup", post(signup_user))
+        .at("/hello/:name", get(hello));
 
+    println!("Server running on port 3000");
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
         .await
